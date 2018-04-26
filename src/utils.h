4b1dd14cd0cc4babf71f6111559c5db394c26872
jmadoremos <21135564+jmadoremos@users.noreply.github.com>
2018-04-26 20:57:42 +0800

    Added fflush to clear CIN

--- src/utils.h
+++ src/utils.h
@@ -1,3 +1,19 @@
+// ********************************* LICENSE ********************************* //
+//   Copyright 2018 James Michael Adoremos
+//
+//   Licensed under the Apache License, Version 2.0 (the "License")
+//   you may not use this file except in compliance with the License
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// *************************************************************************** //
+
 #ifndef _UTILS
 #define _UTILS
 
@@ -9,14 +25,18 @@
 
 // START: Define utility class
 #include <iostream>
+
 using namespace std;
 
-class Utils {
+class Utils
+{
   public:
     static void pause(void);
 };
 
-void Utils::pause(void) {
+void Utils::pause(void)
+{
+  fflush(stdin);
   cout << endl;
   cout << "[INFO] Press any key to continue...";
   cin.get();
